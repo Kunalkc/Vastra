329a370525d9eb28d4e78e0bf9932e37062a02e3
Kunal Chandel <Kunalchandel09@gmail.com>
2025-04-23 22:34:35 +0530

    setup spl routing for react components

--- client/my-app/src/App.jsx
+++ client/my-app/src/App.jsx
@@ -1,34 +1,27 @@
+import react from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './css/Landing.css'
+import './components/Login'
+import './components/Home'
+import './components/Profile'
+import './components/Auction'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className='flex flex-row gap-8'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo bg-amber-400 rounded-2xl" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react bg-red-500 rounded-2xl" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+    <Routes>
+ 
+      <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/auction/:id" element={<Auction />} />
+      
+      {/* Optional: Catch-all route (404 Page Not Found) */}
+      <Route path="*" element={<div>Page Not Found</div>} />
+    </Routes>
+  </Router>
   )
 }
 
