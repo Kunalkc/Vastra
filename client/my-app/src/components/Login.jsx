c5e0b170e63a43d7c17a7b487bb4e1af6061ed83
Kunal Chandel <Kunalchandel09@gmail.com>
2025-04-24 18:09:29 +0530

    minor change

--- client/my-app/src/components/Login.jsx
+++ client/my-app/src/components/Login.jsx
@@ -13,13 +13,13 @@ export default function Login(){
         <div className='box-border h-screen flex flex-col gap-10 justify-center items-center bg-gray-700'>
             <h1 className='text-cyan-100 text-8xl'>VASTRA</h1>
         <div className='p-5 box-border min-w-1/3 login-container w-auto bg-gray-500 rounded-4xl flex flex-col gap-3.5 items-center'>
-           <form className='flex flex-col gap-3 '>
+           <form className='w-[95%] flex flex-col gap-3'>
              <input
                  type='email'
                  placeholder='johndoe@gmail.com'
                  value={email}
                  onChange={(e) => changeid(e.target.value)}
-                 className='bg-amber-50 flex flex-row justify-center'
+                 className='bg-amber-50 '
              />
              <input
                  type='password'
@@ -28,7 +28,7 @@ export default function Login(){
                  onChange={(e) => changepassword(e.target.value)}
                  className='bg-amber-50'
              />
-             <button type='submit' className='bg-gray-700 text-cyan-100 rounded-xl'>Login</button>
+             <button type='submit' className='bg-gray-700 text-cyan-100 rounded-xl w-1/2 self-center'>Login</button>
            </form>
         </div>
         </div>
