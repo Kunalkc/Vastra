4893e568a16e7300192650472f07594af5ee7b07
Kunal Chandel <Kunalchandel09@gmail.com>
2025-05-07 23:04:12 +0530

    further implementation of search

--- client/my-app/src/components/search.jsx
+++ client/my-app/src/components/search.jsx
@@ -6,6 +6,8 @@ export default function searchtab(props){
 
 console.log("searchbar is: " , props.isopen)
 
+    const [products , toggleproduct] = React.useState(true)
+    const [users , toggleusers] = React.useState(false)
 
     return(
         <div className="fixed py-5 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-[70%] h-[80%] bg-gray-800 z-10000 rounded-lg flex flex-col gap-4 justify-between items-center">
